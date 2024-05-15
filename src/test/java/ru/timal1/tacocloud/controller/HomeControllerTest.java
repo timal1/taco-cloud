e169082dcf68c0046c85fd3487e481eeb5c0c6d1
tladatko.adm <tladatko@sitronics.com>
2024-05-15 18:04:29 +0300

    End chapter 2

---
+++ src/test/java/ru/timal1/tacocloud/controller/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package ru.timal1.tacocloud.controller;
+
+import static org.hamcrest.Matchers.containsString;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest()
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("home"))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Welcome to...")));
+    }
+}
+
