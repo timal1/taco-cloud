0d2faa2236cec2ee80a0f850379bba8d34fcb13e
tladatko.adm <tladatko@sitronics.com>
2024-05-15 18:06:57 +0300

    End chapter 2

--- src/main/java/ru/timal1/tacocloud/object/Ingredient.java
+++
@@ -1,8 +0,0 @@
-package ru.timal1.tacocloud.object;
-
-public record Ingredient(String id, String name, Type type) {
-
-    public enum Type {
-        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
-    }
-}
